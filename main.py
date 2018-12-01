94b08436d5505bdf181945044e0a2bedd4aaa730
jetsalinas <joseenrico.salinas@gmail.com>
2018-12-01 12:07:27 +0800

    Fix: typo

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import pyglet
 window = pyglet.window.Window(caption='Pokedex V1.0', width=800, height=600)
 
 # Set blending modes
-pyglet.gl.glEnable(p.gl.GL_BLEND)
-pyglet.gl.glBlendFunc(p.gl.GL_SRC_ALPHA, p.gl.GL_ONE_MINUS_SRC_ALPHA)
+pyglet.gl.glEnable(pyglet.gl.GL_BLEND)
+pyglet.gl.glBlendFunc(pyglet.gl.GL_SRC_ALPHA, pyglet.gl.GL_ONE_MINUS_SRC_ALPHA)
 
 
 @window.event
