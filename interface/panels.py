856482ad6ccbe516ccd80a1ea4cedd7db2262a23
jetsalinas <joseenrico.salinas@gmail.com>
2018-12-01 16:18:15 +0800

    Added stat labels updating

--- interface/panels.py
+++ interface/panels.py
@@ -48,14 +48,12 @@ class InformationPanel():
         self.pokemon_sprite = pyglet.sprite.Sprite(self.pokemon_image, self.background_1_sprite.width, self.background_1_sprite.width, batch=self.front)
 
         # Load labels
-        self.label_name = pyglet.text.Label("Name: {}".format(self.data.name), font_name="Power Clear", x=30, y=440, font_size=12, batch=self.front)
-        self.label_abilities = pyglet.text.Label("Abilities: {}".format(self.data.abilities), font_name="Power Clear", x=30, y=420, font_size=14, batch=self.front)
-        self.label_abilities = pyglet.text.Label("WT: {}".format(self.data.weight), font_name="Power Clear", x=30, y=400, font_size=12, batch=self.front)
-        self.label_abilities = pyglet.text.Label("HT: {}".format(self.data.height), font_name="Power Clear", x=30, y=380, font_size=12, batch=self.front)
+        self.label_name = pyglet.text.Label("Name: {}".format(self.data.name), font_name="Power Clear", x=30, y=440, font_size=15, batch=self.front)
+        self.label_abilities = pyglet.text.Label("Abilities: {}".format(self.data.abilities), font_name="Power Clear", x=30, y=420, font_size=12, batch=self.front)
+        self.label_weight = pyglet.text.Label("WT: {} kg".format(self.data.weight), font_name="Power Clear", x=30, y=400, font_size=12, batch=self.front)
+        self.label_height = pyglet.text.Label("HT: {} m".format(self.data.height), font_name="Power Clear", x=30, y=380, font_size=12, batch=self.front)
 
-    def update_background_sprite(self, type_1, type_2=None):
-        self.type_1 = type_1
-        self.type_2 = None
+    def update_background_sprite(self):
 
         # Replace type_1 sprite background image
         self.background_1_sprite.image = pyglet.image.load('resources/backgrounds/{}.png'.format(self.type_1))
@@ -65,17 +63,23 @@ class InformationPanel():
         if isinstance(self.type_2, str):        
             self.background_2_sprite.image = pyglet.image.load('resources/backgrounds/{}.png'.format(self.type_2))
 
-    def update_pokemon_sprite(self, name):
+    def update_pokemon_sprite(self):
         try:
-            self.pokemon_image = pyglet.image.load('resources/pokemon/{}.jpg'.format(name.lower()))
+            self.pokemon_image = pyglet.image.load('resources/pokemon/{}.jpg'.format(self.data.name.lower()))
         except(FileNotFoundError):
             pass
         try:
-            self.pokemon_image = pyglet.image.load('resources/pokemon/{}.png'.format(name.lower()))
+            self.pokemon_image = pyglet.image.load('resources/pokemon/{}.png'.format(self.data.name.lower()))
         except(FileNotFoundError):
             raise FileNotFoundError("Invalid pokemon name.")
         self.pokemon_image.anchor_x = self.pokemon_image.width
         self.pokemon_sprite.image = self.pokemon_image
+
+    def update_labels(self):
+        self.label_name.text = "Name: {}".format(self.data.name)
+        self.label_abilities.text = "Abilities: {}".format(self.data.abilities)
+        self.label_weight.text = "WT: {} kg".format(self.data.weight)
+        self.label_height.text = "HT: {} m".format(self.data.height)
 
     def update(self, data):
         self.data = data
@@ -84,8 +88,9 @@ class InformationPanel():
         self.type_1 = data.type1
         self.type_2 = data.type2
 
-        self.update_background_sprite(type_1=self.type_1, type_2=self.type_2)
-        self.update_pokemon_sprite(name=self.name)
+        self.update_background_sprite()
+        self.update_pokemon_sprite()
+        self.update_labels()
 
     def draw_self(self):
         self.back.draw()
