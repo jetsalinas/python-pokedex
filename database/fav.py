a04b06714e01f1b5ae8da00d58f16bff747f16f2
jetsalinas <joseenrico.salinas@gmail.com>
2018-12-04 13:43:50 +0800

    Bugfix: fav loading empty strings

--- database/fav.py
+++ database/fav.py
@@ -5,21 +5,21 @@ class Favorites():
     def __init__(self):
         with open("resources/data/fav.csv") as fav_file:
             csv_reader = csv.reader(fav_file)
-            self.favs = {int(x[0]) for x in csv_reader}
+            self.favs = {int(x[0]) for x in csv_reader if x[0] != ''}
 
     def __iter__(self):
         return iter(self.favs)
 
     def append(self, key):
         self.favs = self.favs.union({key})
-        with open("resources/data/fav.csv", "w") as fav_file:
-            csv_writer = csv.writer(fav_file)
+        with open("resources/data/fav.csv", "w", newline='') as fav_file:
+            csv_writer = csv.writer(fav_file, )
             for item in self.favs:
                 csv_writer.writerow([item])
 
     def remove(self, key):
         self.favs = self.favs.difference({key})
-        with open("resources/data/fav.csv", "w") as fav_file:
+        with open("resources/data/fav.csv", "w", newline='') as fav_file:
             csv_writer = csv.writer(fav_file)
             for item in self.favs:
                 csv_writer.writerow([item])
