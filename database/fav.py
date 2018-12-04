171192156dfd6b0d9523c70d3ce54d80befc1903
jetsalinas <joseenrico.salinas@gmail.com>
2018-12-04 19:07:10 +0800

    Added docstrings to fav.py

--- database/fav.py
+++ database/fav.py
@@ -1,8 +1,21 @@
+"""
+Module to save and load favorites for python-pokedex
+
+:Author:    Jose Enrico Salinas
+:Version:   20181204
+"""
+
 import csv
 
 class Favorites():
+    """
+    Handles fav saving and reading
+    """
 
     def __init__(self):
+        """
+        Reads resources/data/fav.csv and loads a set object from it
+        """
         with open("resources/data/fav.csv") as fav_file:
             csv_reader = csv.reader(fav_file)
             self.favs = {int(x[0]) for x in csv_reader if x[0] != ''}
@@ -11,6 +24,12 @@ class Favorites():
         return iter(self.favs)
 
     def append(self, key):
+        """
+        Adds an item to favorites and saves it
+
+        Args:
+            key(str): Index of pokemon to be saved
+        """
         self.favs = self.favs.union({key})
         with open("resources/data/fav.csv", "w", newline='') as fav_file:
             csv_writer = csv.writer(fav_file, )
@@ -18,6 +37,12 @@ class Favorites():
                 csv_writer.writerow([item])
 
     def remove(self, key):
+        """
+        Removes an item from the favorites if it exists and saves the favorites
+
+        Args:
+            key(str): Index of pokemon to be removed
+        """
         self.favs = self.favs.difference({key})
         with open("resources/data/fav.csv", "w", newline='') as fav_file:
             csv_writer = csv.writer(fav_file)
